99716a90332af7060f9ff8a469b528e687599f28
talk2mat2 <62118678+talk2mat2@users.noreply.github.com>
2020-06-13 02:06:40 +0100

    Add files via upload

---
+++ project3/main.js
@@ -0,0 +1,13 @@
+const icon = document.querySelector('.nav-icon');
+const ullist = document.getElementById('ull')
+
+
+
+// icon.addEventListener('click',()=>{
+// ullist.classList.toggle('active')
+// ullist.classList.toggle('ullist')
+// })
+
+icon.addEventListener('click',()=>{
+   document.querySelector('.nav-wrapper').classList.toggle('hide-nav')
+    })
